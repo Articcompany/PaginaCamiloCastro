82cb97aecd6c61e42f94575056321c88e5125a9c
kevkray <kevindiaz9@outlook.com>
2018-04-12 08:56:26 -0500

    agregación del formulario

--- sections/enviarformulario.php
+++ sections/enviarformulario.php
@@ -1,21 +1,26 @@
 <?php
 
 //captura de las variables
-$correo= $_POST["email"];
-$opc= $_POST["opcion"];
-$men= $_POST["mensaje"];
+$correo = $_POST["email"];
+$opc = $_POST["opcion"];
+$men = $_POST["mensaje"];
 
 //estructura del correo
-$destino="articcompanyud@gmail.com";//Correo al que llegará
-$asunto=$_POST["opcion"];//asunto que aparecerá
-$bodyemail="Hola señor Camilo Castro estoy interesado en $opc , mi correo electrónico es $correo y este es mi mensaje $men";//cuerpo del mensaje
+$destino = 'articcompanyud@gmail.com';//Correo al que llegará
+$asunto = $opc;//asunto que aparecerá
+//$bodyemail = "Hola señor Camilo Castro estoy interesado en '$_POST["opcion"]', mi correo electrónico es '$_POST["email"]' y este es mi mensaje: '$_POST["mensaje"]'";//cuerpo del mensaje
 
 //encabezado para que se pueda enviar el correo
-$headers =  'From: '.$destino."\r\n".
-            'Reply-To:'.$destino."\r\n".
-            'X-Mailer: PHP/'.phpversion();
+$headers = 'From:'.$destino."\r\n".
+'Reply-To:'.$destino."\r\n".
+'X-Mailer: PHP/'.phpversion();
 
 //Funcion mail
-mail($destino,$asunto,$bodyemail,$headers);
-echo"Mensaje enviado";
+if(mail($destino,$asunto,$men,$headers)){
+    echo"Mensaje enviado";
+}
+ else {
+    echo"Ha ocurrido algo inesperado";
+ };
+
 ?>
