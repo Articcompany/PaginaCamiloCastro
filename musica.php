26375e1a168a9ba3345c6205b963ccc6c2853c81
digetadr <djcienduaa@gmail.com>
2018-04-18 17:40:40 -0500

    holi

--- musica.php
+++ musica.php
@@ -1,37 +1,29 @@
 <!DOCTYPE html>
 <html class="no-js" lang="en">
-
 <head>
-
     <!--- Aspectos Básicos
     ================================================== -->
     <meta charset="utf-8">
     <title>Camilo Castro</title>
     <meta name="description" content="">
     <meta name="author" content="">
-
     <!-- metas especificos para mobile
     ================================================== -->
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-
     <!-- CSS
     ================================================== -->
     <link rel="stylesheet" href="css/base.css">
     <link rel="stylesheet" href="css/vendor.css">
     <link rel="stylesheet" href="css/main.css">
-
     <!-- script
     ================================================== -->
     <script src="js/modernizr.js"></script>
     <script src="js/pace.min.js"></script>
-
     <!-- favicons
     ================================================== -->
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
     <link rel="icon" href="favicon.ico" type="image/x-icon">
-
 </head>
-
 <body id="top">
     <!-- pageheader
     ================================================== -->
@@ -39,38 +31,27 @@
         include "./sections/header.html";
     ?>
     <!-- header -->
-
     <!-- s-content biografía
     ================================================== -->
-
     <!-- s-content -->
-
     <section class="s-content">
         <div class="row">
             <h1 style="text-align: center;">Musica en soundcloud</h1>
             <iframe width="100%" height="350" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/44185013&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
         </div>
     </section>
-
-
     <?php
         include "./sections/footer.html";
     ?>
-
-
     <!-- preloader
     ================================================== -->
     <?php
         include "./sections/preloader.html";
     ?>
-
-
     <!-- Java Script
     ================================================== -->
     <script src="js/jquery-3.2.1.min.js"></script>
     <script src="js/plugins.js"></script>
     <script src="js/main.js"></script>
-
 </body>
-
 </html>
--- videos.php
+++ videos.php
@@ -1,39 +1,30 @@
 <!DOCTYPE html>
 <html class="no-js" lang="es">
-
 <head>
-
     <!--- Aspectos Básicos
     ================================================== -->
     <meta charset="utf-8">
     <title>Camilo Castro</title>
     <meta name="description" content="Videos producidos por Camilo Castro">
     <meta name="author" content="Artic Company S.A.S.">
-
     <!-- metas specificos para mobile
     ================================================== -->
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-
     <!-- CSS
     ================================================== -->
     <link rel="stylesheet" href="css/base.css">
     <link rel="stylesheet" href="css/vendor.css">
     <link rel="stylesheet" href="css/main.css">
-
     <!-- script
     ================================================== -->
     <script src="js/modernizr.js"></script>
     <script src="js/pace.min.js"></script>
-
     <!-- favicons
     ================================================== -->
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
     <link rel="icon" href="favicon.ico" type="image/x-icon">
-
 </head>
-
 <body id="top">
-
     <!-- pageheader Cabecera
     ================================================== -->
     <?php
@@ -41,35 +32,26 @@
     ?>
     <!-- header -->
     <!-- end s-pageheader -->
-
     <!-- s-content Galeria Videos
     ================================================== -->
-
     <section class="s-content">
-
         <div class="row masonry-wrap">
             <div class="masonry">
-
                 <div class="grid-sizer"></div>
-
                 <article class="masonry__brick entry format-video" data-aos="fade-up">
-
                     <div class="entry__thumb video-image">
                         <a href="https://player.vimeo.com/video/117310401?color=01aef0&title=0&byline=0&portrait=0" data-lity>
                             <img src="images/thumbs/masonry/shutterbug-400.jpg" srcset="images/thumbs/masonry/shutterbug-400.jpg 1x, images/thumbs/masonry/shutterbug-800.jpg 2x" alt="">
                         </a>
                     </div>
-
                     <div class="entry__text">
                         <div class="entry__header">
-
                             <div class="entry__date">
                                 <a href="single-video.html">December 10, 2017</a>
                             </div>
                             <h1 class="entry__title">
                                 <a href="single-video.html">Key Benefits Of Family Photography.</a>
                             </h1>
-
                         </div>
                         <div class="entry__excerpt">
                             <p>
@@ -83,28 +65,22 @@
                             </span>
                         </div>
                     </div>
-
                 </article>
                 <!-- end article -->
-
                 <article class="masonry__brick entry format-video" data-aos="fade-up">
-
                     <div class="entry__thumb video-image">
                         <a href="https://player.vimeo.com/video/117310401?color=01aef0&title=0&byline=0&portrait=0" data-lity>
                             <img src="images/thumbs/masonry/shutterbug-400.jpg" srcset="images/thumbs/masonry/shutterbug-400.jpg 1x, images/thumbs/masonry/shutterbug-800.jpg 2x" alt="">
                         </a>
                     </div>
-
                     <div class="entry__text">
                         <div class="entry__header">
-
                             <div class="entry__date">
                                 <a href="single-video.html">December 10, 2017</a>
                             </div>
                             <h1 class="entry__title">
                                 <a href="single-video.html">Key Benefits Of Family Photography.</a>
                             </h1>
-
                         </div>
                         <div class="entry__excerpt">
                             <p>
@@ -118,29 +94,22 @@
                             </span>
                         </div>
                     </div>
-
                 </article>
                 <!-- end article -->
-
-
                 <article class="masonry__brick entry format-video" data-aos="fade-up">
-
                     <div class="entry__thumb video-image">
                         <a href="https://player.vimeo.com/video/117310401?color=01aef0&title=0&byline=0&portrait=0" data-lity>
                             <img src="images/thumbs/masonry/shutterbug-400.jpg" srcset="images/thumbs/masonry/shutterbug-400.jpg 1x, images/thumbs/masonry/shutterbug-800.jpg 2x" alt="">
                         </a>
                     </div>
-
                     <div class="entry__text">
                         <div class="entry__header">
-
                             <div class="entry__date">
                                 <a href="single-video.html">December 10, 2017</a>
                             </div>
                             <h1 class="entry__title">
                                 <a href="single-video.html">Key Benefits Of Family Photography.</a>
                             </h1>
-
                         </div>
                         <div class="entry__excerpt">
                             <p>
@@ -154,29 +123,22 @@
                             </span>
                         </div>
                     </div>
-
                 </article>
                 <!-- end article -->
-
-
                 <article class="masonry__brick entry format-video" data-aos="fade-up">
-
                     <div class="entry__thumb video-image">
                         <a href="https://player.vimeo.com/video/117310401?color=01aef0&title=0&byline=0&portrait=0" data-lity>
                             <img src="images/thumbs/masonry/shutterbug-400.jpg" srcset="images/thumbs/masonry/shutterbug-400.jpg 1x, images/thumbs/masonry/shutterbug-800.jpg 2x" alt="">
                         </a>
                     </div>
-
                     <div class="entry__text">
                         <div class="entry__header">
-
                             <div class="entry__date">
                                 <a href="single-video.html">December 10, 2017</a>
                             </div>
                             <h1 class="entry__title">
                                 <a href="single-video.html">Key Benefits Of Family Photography.</a>
                             </h1>
-
                         </div>
                         <div class="entry__excerpt">
                             <p>
@@ -190,29 +152,22 @@
                             </span>
                         </div>
                     </div>
-
                 </article>
                 <!-- end article -->
-
-
                 <article class="masonry__brick entry format-video" data-aos="fade-up">
-
                     <div class="entry__thumb video-image">
                         <a href="https://player.vimeo.com/video/117310401?color=01aef0&title=0&byline=0&portrait=0" data-lity>
                             <img src="images/thumbs/masonry/shutterbug-400.jpg" srcset="images/thumbs/masonry/shutterbug-400.jpg 1x, images/thumbs/masonry/shutterbug-800.jpg 2x" alt="">
                         </a>
                     </div>
-
                     <div class="entry__text">
                         <div class="entry__header">
-
                             <div class="entry__date">
                                 <a href="single-video.html">December 10, 2017</a>
                             </div>
                             <h1 class="entry__title">
                                 <a href="single-video.html">Key Benefits Of Family Photography.</a>
                             </h1>
-
                         </div>
                         <div class="entry__excerpt">
                             <p>
@@ -226,29 +181,22 @@
                             </span>
                         </div>
                     </div>
-
                 </article>
                 <!-- end article -->
-
-
                 <article class="masonry__brick entry format-video" data-aos="fade-up">
-
                     <div class="entry__thumb video-image">
                         <a href="https://player.vimeo.com/video/117310401?color=01aef0&title=0&byline=0&portrait=0" data-lity>
                             <img src="images/thumbs/masonry/shutterbug-400.jpg" srcset="images/thumbs/masonry/shutterbug-400.jpg 1x, images/thumbs/masonry/shutterbug-800.jpg 2x" alt="">
                         </a>
                     </div>
-
                     <div class="entry__text">
                         <div class="entry__header">
-
                             <div class="entry__date">
                                 <a href="single-video.html">December 10, 2017</a>
                             </div>
                             <h1 class="entry__title">
                                 <a href="single-video.html">Key Benefits Of Family Photography.</a>
                             </h1>
-
                         </div>
                         <div class="entry__excerpt">
                             <p>
@@ -262,36 +210,26 @@
                             </span>
                         </div>
                     </div>
-
                 </article>
                 <!-- end article -->
-
             </div>
             <!-- end masonry -->
         </div>
         <!-- end masonry-wrap -->
     </section>
     <!-- s-content -->
-
-
     <?php
         include "./sections/footer.html";
     ?>
-
-
     <!-- preloader
     ================================================== -->
     <?php
         include "./sections/preloader.html";
     ?>
-
-
     <!-- Java Script
     ================================================== -->
     <script src="js/jquery-3.2.1.min.js"></script>
     <script src="js/plugins.js"></script>
     <script src="js/main.js"></script>
-
 </body>
-
 </html>
